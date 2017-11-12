3e0bfc30eb162b7f593289c2a4be5e66d3f3930f
Adam Rahman <ar@adamr.io>
2017-11-12 01:18:30 -0500

    Break lines

--- description.py
+++ description.py
@@ -3,12 +3,12 @@ is mass incarceration. Mass incarceration refers to the substantial \
 increase of incarcerated individuals in the United States over the \
 past four decades, reflective of not only the country's \
 excessively punitive criminal justice system, but also its \
-perpetuation of oppression along racial lines. The sliders \
+perpetuation of oppression along racial lines. \n The sliders \
 represent different circumstances the user can change about \
 the simulation. The population of the simulation can be increased \
-or decreased. The average sentence length refers to how long on average \
+or decreased. \n The average sentence length refers to how long on average \
 the population will stay in prison and the longer the sentence, the more \
-"contagious" incarceration is in the simulation. The race of the \
+"contagious" incarceration is in the simulation. \n The race of the \
 simulated population can be switched between black and white. This is to \
 illustrate the difference of the incarceration epidemic between black \
 and white populations'''
