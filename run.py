a4936affec39b62c5f504c2b9fbd8d054e6a27ac
Adam Rahman <ar@adamr.io>
2017-11-19 12:52:51 -0500

    Attempted Heroku deploy

--- run.py
+++ run.py
@@ -6,30 +6,6 @@
 # :................................................................:
 
 import os
-import tornado.autoreload
-import tornado.ioloop
-import tornado.web
-import tornado.websocket
-import tornado.escape
-import tornado.gen
-import webbrowser
-import tornado.httpserver
-
 from server import server
-#server.launch()
-
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello world")
-
-def main():
-    application = tornado.web.Application([
-        (r"/", MainHandler),
-    ])
-    http_server = tornado.httpserver.HTTPServer(application)
-    port = int(os.environ.get("PORT", 5000))
-    http_server.listen(port)
-    tornado.ioloop.IOLoop.instance().start()
- 
-if __name__ == "__main__":
-    main()
+server.port(int.os.environ.get("PORT",5000))
+server.Launch()
